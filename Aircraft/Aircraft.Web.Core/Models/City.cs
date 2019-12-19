77a7f4c8acb4bb6fe41326c3ca66856caaa25ba3
Dmitri <lipov758@gmail.com>
2019-12-20 01:19:16 +0300

    UI update

---
+++ Aircraft/Aircraft.Web.Core/Models/City.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace Aircraft.Web.Core.Models
+{
+    public class City:Base
+    {
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+    }
+}
---
+++ Aircraft/Aircraft.Web/Controllers/DateController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aircraft.Web.Controllers
+{
+    [Route("api/{controller}")]
+    public class DateController : Controller
+    {
+        [HttpGet("GetCurrentDate")]
+        public DateTimeOffset GetCurrentDate()
+        {
+            return DateTimeOffset.Now;
+        }
+    }
+}
