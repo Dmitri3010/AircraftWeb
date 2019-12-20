95a0c5b248a74790ab74992732473ce4e56358ed
Dmitri <lipov758@gmail.com>
2019-12-20 12:45:23 +0300

    Add flight controller

--- Aircraft/Aircraft.Web/Controllers/FlightController.cs
+++ Aircraft/Aircraft.Web/Controllers/FlightController.cs
@@ -1,7 +1,40 @@
-﻿namespace Aircraft.Web.Controllers
+﻿using System.Linq;
+using Aircraft.Web.Core.Models;
+using Microsoft.AspNetCore.Mvc;
+using DB = PN.Storage.EF.SimpleRepository;
+
+namespace Aircraft.Web.Controllers
 {
-    public class FlightController
+    [Route("api/[controller]")]
+    public class FlightController:Controller
     {
-        
+        [HttpGet("GetFlights")]
+        public ActionResult GetFlights()
+        {
+            return Json(DB.Get<Flight>());
+        }
+
+        [HttpGet("GetFlight")]
+        public ActionResult GetFlight(string flightId)
+        {
+            if (string.IsNullOrWhiteSpace(flightId))
+                return BadRequest();
+            return Ok(DB.Get<Flight>().Where(p => p.Id.ToString() == flightId));
+        }
+
+        [HttpGet("DeleteFlight")]
+        public ActionResult DeleteFlight(string flightId)
+        {
+            var flight = DB.Single<Plane>(p => p.Id.ToString() == flightId);
+            DB.Delete(flight);
+            return Ok();
+        }
+
+        [HttpPost("AddOrUpdate")]
+        public ActionResult AddOrUpdate([FromBody] Flight flight)
+        {
+            DB.Upsert(flight);
+            return Ok(new ResponseStatus() {Status = "200", Message = ""});
+        }
     }
 }
